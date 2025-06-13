2c64e6a9279ce2e970a5566df87f768e519da57e
Luis Frade <luis.carvalho.frade@gmail..com>
2025-06-12 21:14:40 -0300

    updating with some study and also start to write pseudo code

--- philo/philo.c
+++ philo/philo.c
@@ -6,18 +6,47 @@
 /*   By: luide-ca <luide-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 00:13:02 by luide-ca          #+#    #+#             */
-/*   Updated: 2025/06/10 09:58:11 by luide-ca         ###   ########.fr       */
+/*   Updated: 2025/06/12 21:10:20 by luide-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+
+int	moonitor()
+{
+
+}
+
+int	run_simulation(void)
+{
+	/*
+		1. set the table
+		while (number of times to eat || is_everyboy alive)
+		{
+			2. nth philo get the right and the left forks
+				2.1. start to count the time for the nth philo to eat
+				2.2. start to count the time to the others to not to die
+				2.3. nth philo leaves the forks on the table
+				2.4. time for nth philo not to die starts
+		}
+		3. monitor checks with anybody died, if it is true finish program
+	*/
+}
+
+
 int	main(int argc, char **argv)
 {
-	if (argc < 5 && argc > 6)
-	{
-		printf("ERROR imput");
-		return (1);
-	}
+
+	if (argc < 5 || argc > 6)
+		printf("Error");
+	// validate if all inputs are of the right type
+
+	// transform the argv into integers
+	
+	// init instances of philosophers
+	init_philo();
+	// run the simulation
+	run_simulation();
 	return (0);
 }
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: luide-ca <luide-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 00:13:06 by luide-ca          #+#    #+#             */
-/*   Updated: 2025/06/10 09:58:26 by luide-ca         ###   ########.fr       */
+/*   Updated: 2025/06/12 20:58:43 by luide-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,21 @@
 # include <pthread.h>
 # include <stdio.h>
 
+typedef struct  s_infos
+{
+    int         num_philos;
+    int         num_forks;
+    int         time_eat;
+    int         time_sleep;
+    int         time_think;
+    pthread_t   thread;
+}               t_infos;
+
+typedef struct  s_philo
+{
+    int right_fork;
+    int left_fork;
+    int philo_num;
+}               t_philo;
 
 #endif
