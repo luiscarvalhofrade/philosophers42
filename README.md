67430111333024611361b7491ef93b42ac03c15d
luis frade <luis.carvalho.frade@gmail.com>
2025-06-16 18:32:26 -0300

    updating readme

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # philosophers42
+
+	first, i need to imagine how to make the dyanmic between philosophers, each one
+	is the first when he gets the first and the second fork and so on. Second, how
+	to deal with other philos, if anyother is set whatsoever.
+
+	1. set each philosopher: unique number
+
+	2. set forks: unique number
+
+	here the nth philo gets the nth and the (n + 1)th fork, the (n + 1)th philo, gets
+	tje (n + 1)th plus the (n + 2)th forks and so on.
+
+	if we have t philos:
+
+	#1 philo -> #1 and #2 forks
+	#2 philo -> #2 and #3 forks
+	.
+	.
+	.
+	#n philo -> #n and #(n+1) forks
+	.
+	.
+	.
+	#(t-1) philo -> #(t-1) and #t forks
+	#t philo -> #t and #1 forks
+
+	#1 philo gets forks #1 and #2, all other philos start to count the time?
+	#1 start to eats, all other wait 200 miliseconds
--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: luide-ca <luide-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 00:13:02 by luide-ca          #+#    #+#             */
-/*   Updated: 2025/06/16 17:42:26 by luide-ca         ###   ########.fr       */
+/*   Updated: 2025/06/16 18:25:46 by luide-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,31 @@
 /*
 	first, i need to imagine how to make the dyanmic between philosophers, each one
 	is the first when he gets the first and the second fork and so on. Second, how
-	to deal with the others philos, if anyother is set whatsoever.
+	to deal with other philos, if anyother is set whatsoever.
 
+	1. set each philosopher: unique number
+
+	2. set forks: unique number
+
+	here the nth philo gets the nth and the (n + 1)th fork, the (n + 1)th philo, gets
+	tje (n + 1)th plus the (n + 2)th forks and so on.
+
+	if we have t philos:
+
+	#1 philo -> #1 and #2 forks
+	#2 philo -> #2 and #3 forks
+	.
+	.
+	.
+	#n philo -> #n and #(n+1) forks
+	.
+	.
+	.
+	#(t-1) philo -> #(t-1) and #t forks
+	#t philo -> #t and #1 forks
+
+	#1 philo gets forks #1 and #2, all other philos start to count the time?
+	#1 start to eats, all other wait 200 miliseconds
 */
 
 int	monitor()
